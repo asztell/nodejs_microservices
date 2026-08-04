c0ec5d77bb3b834fc3ca9adb1b94c70adfffe881
Arpad Asztalos <asztell@ucla.edu>
2026-08-04 17:23:43 -0400

    initial setup and shared code

---
+++ packages/shared/src/errors/errorHandler.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./AppError";
+
+export function errorHandler(
+  err: unknown,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
+  return res.status(500).json({
+    soccess: false,
+    message: "Internal server error",
+  });
+}
