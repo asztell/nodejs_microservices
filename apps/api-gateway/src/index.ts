1b0a9c93ff6f4d3275d25e74b8c81657a5e5468e
Arpad Asztalos <asztell@ucla.edu>
2026-08-05 20:19:18 -0400

    add api-gateway

---
+++ apps/api-gateway/src/index.ts
@@ -0,0 +1,59 @@
+import { config } from "dotenv";
+import { resolve } from "node:path";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
+import {
+  AppError,
+  errorHandler,
+  httpLogger,
+  logger,
+  successResponse,
+} from "shared";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+config({ path: resolve(process.cwd(), ".env") });
+config({ path: resolve(process.cwd(), "../../.env") });
+
+const { PORT = 3000, AUTH_SERVICE_URL = "http://127.0.0.1:3001" } = process.env;
+
+const app = express();
+
+// secure http headers
+app.use(helmet());
+app.use(cors());
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  }),
+);
+
+app.use(httpLogger);
+
+app.use("/health", (_req, res) => {
+  successResponse(res, { service: "api-gateway" });
+});
+
+app.use(
+  "/auth",
+  createProxyMiddleware({
+    target: AUTH_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: (path) => `/auth${path}`,
+  }),
+);
+
+app.use((_req, _res, next) => {
+  next(new AppError(404, "Route not found"));
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`API gateway running on port ${PORT}`);
+});
